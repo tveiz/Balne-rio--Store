3d87e63adec03dfa352d5f0190505314ef32d6d7
tveiz <tm9034156@gmail.com>
2026-01-09 22:35:27 -0300

    Create client.ts

---
+++ lib/supabase/client.ts
@@ -0,0 +1,11 @@
+import { createBrowserClient } from "@supabase/ssr"
+
+export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://zhuqyenmojtoymnvmfza.supabase.co"
+
+  const supabaseKey =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpodXF5ZW5tb2p0b3ltbnZtZnphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5OTE0OTEsImV4cCI6MjA4MzU2NzQ5MX0.2HALniJb3rOfPBYjjRJblsvWJ4MToYYkYc2joj0kPhQ"
+
+  return createBrowserClient(supabaseUrl, supabaseKey)
+}
